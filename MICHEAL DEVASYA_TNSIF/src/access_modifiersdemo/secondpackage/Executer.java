ddaf152d01de5bc421638860118c0518392a05c4
starn <starn@DESKTOP-PM7KMRB>
2025-07-05 17:34:05 +0530

    Commit

---
+++ MICHEAL DEVASYA_TNSIF/src/access_modifiersdemo/secondpackage/Executer.java
@@ -0,0 +1,16 @@
+package access_modifiersdemo.secondpackage;
+
+import access_modifiersdemo.firstpackage.Base;
+
+public class Executer {
+
+	public static void main(String[] args) {
+		Base b1 = new Base();
+		b1.methodPublic();
+		b1.varPublic = 41;
+		b1.methodPublic();
+		
+
+	}
+
+}
